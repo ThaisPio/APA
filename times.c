038456f4113b691d79ea2d14a45a46cd9fa6ed28
ThaisPio <89753258+ThaisPio@users.noreply.github.com>
2024-09-24 06:28:52 -0300

    Update times.c

--- times.c
+++ times.c
@@ -35,37 +35,30 @@ void ordenarPorPontos(struct Time times[], int inicio, int fim) {
 int main() {
     int opcao, pontosBusca, i; 
     struct Time tabela[] = {
-        {"Palmeiras", 63},
-        {"Flamengo", 63},
-        {"Botafogo", 62},
-        {"Atletico-MG", 60},
-        {"Gremio", 59},
-        {"Bragantino", 59},
-        {"Fluminense", 53},
-        {"Atletico - PR", 52},
-        {"Cuiba", 48},
-        {"Sao Paulo", 47},
-        {"Internacional", 46},
-        {"Fortaleza", 45},
-        {"Cruzeiro", 44},
-        {"Corinthians", 44},
-        {"Santos", 43},
-        {"Vasco", 42},
-        {"Bahia", 41},
-        {"Goias", 35},
-        {"Coritiba", 29},
-        {"America-MG", 21}
+        {"Argentina Grupo A", 9},
+        {"Canada Grupo A", 4},
+        {"Chile Grupo A", 2},
+        {"Peru Grupo A", 1},
+        {"Venezuela Grupo B", 9},
+        {"Equador Grupo B", 4},
+        {"Mexico Grupo B", 4},
+        {"Jamaica Grupo B", 0},
+        {"Uruguai Grupo C", 9},
+        {"Panama Grupo C", 6},
+        {"Estados Unidos Grupo C", 3},
+        {"Colombia Grupo D", 7},
+        {"Brasil Grupo D", 5},
+        {"Costa Rica Grupo D", 4},
+        {"Paraguai Grupo D", 0}
+        
     };
 
     do {
-        printf("\nMenu de Atividades:\n");
-        printf("1. Retornar o Nome e Posicao do Time com base na quantidade de pontos\n");
-        printf("2. Exibir os 5 primeiros colocados\n");
-        printf("3. Exibir os 5 ultimos colocados\n");
-        printf("4. Exibir a diferenca de pontos entre os 5 primeiros\n");
-        printf("5. Exibir a diferenca de pontos entre os 5 ultimos\n");
-        printf("6. Reordenar a tabela para exibir apenas metade dos times com base na quantidade de pontos\n");
-        printf("0. Sair\n");
+        printf("1. Retornar o Nome com base na quantidade de pontos\n");
+        
+        printf("2. Reordenar a tabela para exibir os times com base na quantidade de pontos\n");
+        
+		printf("3. Sair\n");
 
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
@@ -75,43 +68,23 @@ int main() {
                 printf("Informe a quantidade de pontos: ");
                 scanf("%d", &pontosBusca);
 
-                for (i = 0; i < sizeof(tabela) / sizeof(tabela[0]); i++) {
+            for (i = 0; i < sizeof(tabela) / sizeof(tabela[0]); i++) {
                     if (tabela[i].pontos == pontosBusca) {
-                        printf("Posicao: %d, Time: %s\n", i + 1, tabela[i].nome_Time);
-                    }
+                        printf("Time: %s\n", tabela[i].nome_Time);
+                   }
                 }
                 break;
 
             case 2:
-                for (i = 0; i < 5; i++) {
-                    printf("Posicao: %d, Time: %s, Pontos: %d\n", i + 1, tabela[i].nome_Time, tabela[i].pontos);
-                }
-                break;
-
-            case 3:
-                for (i = sizeof(tabela) / sizeof(tabela[0]) - 5; i < sizeof(tabela) / sizeof(tabela[0]); i++) {
-                    printf("Posicao: %d, Time: %s, Pontos: %d\n", i + 1, tabela[i].nome_Time, tabela[i].pontos);
-                }
-                break;
-
-            case 4:
-                printf("Diferenca de pontos entre os 5 primeiros: %d\n", tabela[0].pontos - tabela[4].pontos);
-                break;
-
-            case 5:
-                printf("Diferenca de pontos entre os 5 ultimos: %d\n", tabela[sizeof(tabela) / sizeof(tabela[0]) - 5].pontos - tabela[sizeof(tabela) / sizeof(tabela[0]) - 1].pontos);
-                break;
-
-            case 6:
                 ordenarPorPontos(tabela, 0, sizeof(tabela) / sizeof(tabela[0]) - 1);
-                int metade = sizeof(tabela) / sizeof(tabela[0]) / 2;
+                int metade = sizeof(tabela) / sizeof(tabela[0]);
 
                 for (i = 0; i < metade; i++) {
                     printf("Posicao: %d, Time: %s, Pontos: %d\n", i + 1, tabela[i].nome_Time, tabela[i].pontos);
                 }
                 break;
 
-            case 0:
+            case 3:
                 printf("Saindo do programa.\n");
                 break;
 
@@ -123,3 +96,4 @@ int main() {
 
     return 0;
 }
+
